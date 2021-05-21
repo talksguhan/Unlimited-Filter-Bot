aff20aa2e92c8fe0b3ae827e70e3eabe7336ebb4
talksguhan <84527006+talksguhan@users.noreply.github.com>
2021-05-21 10:39:44 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -243,12 +243,12 @@ async def help(client, message):
         reply_markup=InlineKeyboardMarkup(
             [
                 [
-                    InlineKeyboardButton("How to Deploy?", url="https://youtu.be/hkmc3e7U7R4"),
+                    InlineKeyboardButton("Channel 🔰", url="https://t.me/Tamil_Tentkotta"),
                     InlineKeyboardButton("About Me", callback_data="about_data")
                 ],
                 [
-                    InlineKeyboardButton("BOT Channel", url="https://t.me/TroJanzHEX"),
-                    InlineKeyboardButton("Support Group", url="https://t.me/TroJanzSupport")
+                    InlineKeyboardButton("Channel🔰", url="https://t.me/Tamil_Tentkotta"),
+                    InlineKeyboardButton("Group♻", url="https://t.me/TamilTentkotta_Chat")
                 ]
             ]
         ),
@@ -265,7 +265,7 @@ async def about(client, message):
             [
                 [
                     InlineKeyboardButton(
-                        "SOURCE CODE", url="https://github.com/TroJanzHEX/Unlimited-Filter-Bot")
+         
                 ],
                 [
                     InlineKeyboardButton("BACK", callback_data="help_data"),
